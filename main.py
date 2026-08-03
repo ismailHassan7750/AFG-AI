ff9b1a960ed02329020c1b587a87bd202f02807c
Ismail Hassan <--GitHub-@example.com>
2026-08-03 10:08:42 +0430

    debug openrouter

--- main.py
+++ main.py
@@ -220,7 +220,8 @@ def ai_response(message, owner_key="", history=[]):
             saved_chat,
             message
         )
-
+print("USING MODEL: google/gemini-2.5-flash")
+print("KEY START:", os.getenv("OPENROUTER_API_KEY")[:10])
         response = client.chat.completions.create(
             model="google/gemini-2.5-flash",
             temperature=0.9,
